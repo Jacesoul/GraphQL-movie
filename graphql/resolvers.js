f49bf121bc23a1a6fa8a8588088b98617512d54e
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-04 15:29:21 +0900

    Movie Suggestions

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
-import { getMovies } from "../database/db";
+import { getMovies, getMovie, getSuggestions } from "../database/db";
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
