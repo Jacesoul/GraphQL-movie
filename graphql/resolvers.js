80189d01c4751ef9122dc99fd4fa785bbfedf42b
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-04 13:58:43 +0900

    Delete Mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,4 @@
-import { getMovies, getById, addMovie } from "../database/db";
+import { getMovies, getById, addMovie, deleteMovie } from "../database/db";
 
 const resolvers = {
   Query: {
@@ -7,6 +7,7 @@ const resolvers = {
   },
   Mutation: {
     addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
